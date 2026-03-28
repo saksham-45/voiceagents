3947070a5d852210af7949f04601715d0cde39c1
anubhav-gupta-software <anubhavgupta.main@gmail.com>
2026-03-28 17:37:55 -0400

    organized into a folder so saksham could add more stuff

--- backend/wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "WRAPPER STARTED at $(date) DIR=$DIR" >> /tmp/voiceagent_debug.log
-exec "$DIR/venv/bin/python" -u "$DIR/host.py" 2>>"$DIR/wrapper.err"
